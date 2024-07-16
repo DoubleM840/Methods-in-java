186fea998af45c2e5ce4784ebb5f98a88057be1d
Judy Mwirigi <double18m22@gmail.com>
2024-07-16 21:13:40 +0300

    Add files via upload

---
+++ Methods_in_java.java
@@ -0,0 +1,52 @@
+//package com.mycompany.methods_in_java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Group A
+ */
+public class Methods_in_java {
+
+    public static void main(String[] args) {
+        findLargestAndSmallest();
+    }
+
+    public static void findLargestAndSmallest() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter the third number: ");
+        double num3 = scanner.nextDouble();
+
+        double largest;
+        double smallest;
+
+        // Determine the largest number
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+
+        // Determine the smallest number
+        if (num1 <= num2 && num1 <= num3) {
+            smallest = num1;
+        } else if (num2 <= num1 && num2 <= num3) {
+            smallest = num2;
+        } else {
+            smallest = num3;
+        }
+
+        System.out.println("The smallest number: " + smallest);
+        System.out.println("The largest number: " + largest);
+        System.out.println(largest + " is your largest and " + smallest + " is your smallest number.");
+    }
+}
